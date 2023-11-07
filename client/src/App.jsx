454931e1490cc544030595e4f9f8e90d500a18ba
Ioanna Ratayova <117192715+ioannaratayova@users.noreply.github.com>
2023-11-07 13:01:23 +0200

    Add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,18 +14,18 @@ function App() {
     }
     return (
         <AuthContext.Provider value={{auth, onLoginSubmit}}>
-        <div>
-            <Nav/>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/catalog' element={<h1>catalog</h1>} />
-                <Route path='/catalog/:itemId' element={<h1>catalog s id</h1>} />
-                <Route path='/myitems' element={<h1>My items</h1>} />
-                <Route path='/login' element={<h1>Login</h1>} />
-                <Route path='/register' element={<h1>Register</h1>} />
-                <Route path='/logout' element={<h1>Logout</h1>} />
-            </Routes>   
-        </div>
+            <div>
+                <Nav/>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/catalog' element={<h1>catalog</h1>} />
+                        <Route path='/catalog/:itemId' element={<h1>catalog s id</h1>} />
+                        <Route path='/myitems' element={<h1>My items</h1>} />
+                        <Route path='/login' element={<h1>Login</h1>} />
+                        <Route path='/register' element={<h1>Register</h1>} />
+                        <Route path='/logout' element={<h1>Logout</h1>} />
+                    </Routes>   
+            </div>
         </AuthContext.Provider>
     )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,42 @@
+import bookPhoto from '../static/book-photo-home.png'
+import './Home.css'
+
+
 export const Home = () => {
-    return <h1>home page</h1>;
+    return (
+        <section className="slider_section">
+            <div className="slider_container">
+                
+                <div
+                    id="carouselExampleIndicators"
+                    className="carousel slide"
+                    data-ride="carousel"
+                >
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-md-7">
+                                        <div className="detail-box">
+                                            <h1>
+                                                Welcome To Our <br />
+                                                Auction
+                                            </h1>
+                                            <p>
+                                                Here you can buy your dream book. 
+                                            </p>
+                                            <a href="">Contact Us</a>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-5 ">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
